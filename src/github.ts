f9edcb8b37b5c7b8a74582f624523706102f0bbe
SelvaCanabady P <canabady@gmail.com>
2020-08-28 13:46:56 +0530

    Adds helper functions to format text as Markdown

---
+++ src/github.ts
@@ -0,0 +1,27 @@
+import * as github from '@actions/github'
+import * as version from './version'
+import * as markdown from './markdown'
+
+export aync function createReleaseDraft(versionTag: string, repoToken: string, changeLog: string): Promise<string>{
+
+    const octokit = new github.GitHub(repoToken)
+
+    const reponse = await octokit.repos.createRelease({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        tag_name: versionTag,
+        name: version.removePrefix(versionTag),
+        body: markdown.toUnorderList(changeLog),
+        prerelease: version.isPrerelease(versionTag,
+        draft: true)
+    })
+  
+    if( response.status != 201 )
+    {
+        throw new Error(`Failed to create the release: ${reponse.status}`)
+    }
+
+    core.info(`Create release draft ${response.data.name}`)
+
+    return reponse.data.html_url
+}
