8cbca551e72d822c789cc024283a72f6b68c405b
SelvaCanabady P <canabady@gmail.com>
2020-08-28 13:52:37 +0530

    Creates a GitHub release draft based on the created tag

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import * as core from '@actions/core'
+import * as event from './event'
+import * as version from './version'
 
 export async function run(): Promise<void> {
   try {
-      core.setOutput('release-url', 'https://www.example.com/')
+      const token = core.getInput('repo-token')
+      const tag = event.getCreatedTag()
+
+      var releaseUrl = ''
+
+      if ( tag && version.isSemVer(tag) )
+      {
+          const changelog = await git.getChangesIntroducedByTag(tag)
+          releaseUrl = await github.createReleaseDraft(tag, token, changelog)
+
+      }
+
+      core.setOutput('release-url', releaseUrl)
   } catch (error) {
     core.setFailed(error.message)
   }
